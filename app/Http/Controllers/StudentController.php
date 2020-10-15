447d7ab6e8a0435018f98e8fb402981189c2c136
federicov <vaglicafederico95@gmail.com>
2020-10-15 15:39:50 +0200

    Final commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,27 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Student;
 class StudentController extends Controller
 {
-
-    protected $students;
-
-    public function __construct(){
-        $this -> students = config('students');
-    }
     public function studenti(){
-        $data = $this->students;
-
+        $data = Student::all();
         return view('studenti',compact('data'));
+
     }
 
-    public function show($id){
-        if(!array_key_exists($id,$this->students)){
-            abort(404);
-        }
 
-        $student = $this->students[$id];
-        return view('show',compact('student'));
-    }
+    // protected $students;
+
+    // public function __construct(){
+    //     $this -> students = config('students');
+    // }
+    // public function studenti(){
+    //     $data = $this->students;
+
+    //     return view('studenti',compact('data'));
+    // }
+
+    // public function show($id){
+    //     if(!array_key_exists($id,$this->students)){
+    //         abort(404);
+    //     }
+
+    //     $student = $this->students[$id];
+    //     return view('show',compact('student'));
+    // }
 }
--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -7,11 +7,11 @@
 <div class="card-deck ">
         @foreach ($data as $key => $student)
             <div class="card container-card">
-            <img class="card-img-top" src="{{$student['img']}}" alt="{{$student['nome']}}">
+            <img class="card-img-top" src="{{$student->img}}" alt="{{$student->nome}}">
             <div class="card-body">
-                <a href="{{route('Student.show',['id'=>$key])}}"><h5 class="card-title">{{$student['nome']}}</h5></a>
-                <p class="card-text">{{$student['descrizione']}}</p>
-                <p class="card-text"><small class="text-muted">{{$student['anni']}}</small></p>
+                <a href="{{route('Student.show',['id'=>$student->id])}}"><h5 class="card-title">{{$student->name}}</h5></a>
+                <p class="card-text">{{$student->descrizione}}</p>
+                <p class="card-text"><small class="text-muted">{{$student->anni}}</small></p>
             </div>
             </div>
         @endforeach
